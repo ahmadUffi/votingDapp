74d892eee038a1e2e0547b4d9ac1bc2018496b44
ahmadUffi <ahmaduffi45@gmail.com>
2025-05-16 20:34:00 +0700

    show profile in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import { Button, Typography } from "@mui/material";
 import React, { useState } from "react";
 import { NavLink } from "react-router";
 
-const Navbar = ({ setIsOpenProfile, account, connectMetaMask }) => {
+const Navbar = ({
+  setIsOpenProfile,
+  account,
+  connectMetaMask,
+  shortAddress,
+  profile,
+}) => {
+  console.log("navbar", account);
   return (
     <div className="navbar fixed top-0 left-0 right-0 flex justify-between items-center p-4 bg-gray-200 text-white w-full z-10">
       <div className="navigation flex">
@@ -21,11 +28,15 @@ const Navbar = ({ setIsOpenProfile, account, connectMetaMask }) => {
       {account ? (
         <div className="profile flex gap-2 justify-center items-center">
           <Typography variant="p" className="text-md" color="blue">
-            ssdsf3...rwfe23
+            {shortAddress}
           </Typography>
           <div className="photo w-[60px] h-[60px] rounded-full bg-red-50 overflow-hidden">
             <img
-              src="https://via.placeholder.com/40"
+              src={
+                profile?.imgProfile
+                  ? profile?.imgProfile
+                  : `https://api.dicebear.com/9.x/pixel-art/svg`
+              }
               alt="Profile"
               className="bg-center bg-cover rounded-full"
             />
